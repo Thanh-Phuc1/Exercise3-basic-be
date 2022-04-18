b27b82584020949557a63ba8e2f62fce93b807c4
thanh phuc <100560476+Thanh-Phuc1@users.noreply.github.com>
2022-04-18 23:17:09 +0700

    "add students"

--- routes/student.api.js
+++ routes/student.api.js
@@ -1,28 +1,34 @@
-const data = {};
+let data = {};
 const express = require("express");
 const router = express.Router();
 
-/* GET students. */
-router.get("/", function (req, res, next) {
-  // const queries = req.query;
-  // console.log(queries);
-  // test error
-  // try {
-  //   const x = 3;
-  //   x.map((e) => e);
-  //   return res.status(200).send({ data });
-  // } catch (error) {
-  //   next(error);
-  // }
+const fs = require("fs");
 
-  return res.status(200).send("students");
+router.get("/", function (req, res, next) {
+  let response;
+
+  try {
+    response = fs.readFileSync("./db.json", "utf8");
+    response = JSON.parse(response);
+  } catch (error) {
+    console.log(error);
+  }
+  return res.status(200).send(response);
 });
 
 /* GET students. */
 router.get("/:id", function (req, res, next) {
-  // const params = req.params;
+  // const id = req.params.id;
   // console.log(params);
-  return res.status(200).send({ data });
+  let response;
+  try {
+    response = fs.readFileSync("./db.json", "utf8");
+    response = JSON.parse(response);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return res.status(200).send(response.filter((e) => e.id === req.params.id));
 });
 
 module.exports = router;
